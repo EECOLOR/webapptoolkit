393b707dfa77112bcf38d27de1f9cf480621d4c0
EECOLOR <EECOLOR@39327bfd-a587-c501-7219-2bb99fe1d7e3>
2011-10-16 10:10:00 +0000

    Moved freemarker.metadata to freemarker.utils

--- webAppToolkit/rendering.freemarker.metadata/trunk/src/main/java/ee/webAppToolkit/freemarker/metadata/expert/CustomObjectTemplateModelFactory.java
+++
@@ -1,8 +0,0 @@
-package ee.webAppToolkit.freemarker.metadata.expert;
-
-import freemarker.template.ObjectWrapper;
-
-public interface CustomObjectTemplateModelFactory
-{
-	public CustomObjectTemplateModel create(Object object, ObjectWrapper objectWrapper);
-}
